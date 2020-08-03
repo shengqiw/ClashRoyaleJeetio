818fa093e059632c3a730b6dd6aa97d9029266d0
Shengqi Wang <shengqiw97@gmail.com>
2020-08-02 20:52:08 -0400

    test reactGA

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,25 @@ import Promotion from './promotion'
 import Guides from './guides'
 import Tournament from './tournament'
 import { ScrollToTop } from './scroll-top'
+import createHistory from 'history/createBrowserHistory'
+import ReactGA from 'react-ga'
 
-export default function App() {
+const history = createHistory()
+history.listen(location => {
+    ReactGA.set({ page: location.pathname })
+    ReactGA.pageview(location.pathname)
+})
+
+
+export default class App extends Component {
+    componentDidMount() {
+        ReactGA.pageview(location.pathname)
+    }
 
 
 
-    const [clanData, setClanData] = useState({});
-    const TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ2ZjhmNjk3LTRmYzItNDJmOC1hODU0LWU1YzgyNGI0NDA5ZiIsImlhdCI6MTU4NzMxODQ2OCwic3ViIjoiZGV2ZWxvcGVyLzExMTU1OTNkLTgwZDctYThjMS01MTA3LTY1OTQwODIxNDI0MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3NS4xNjIuNDguMzEiXSwidHlwZSI6ImNsaWVudCJ9XX0.kYiHJ20k12BplKU8M8oBRRv9U-buwtYKH1LiTa6_x1qpqmeRP31E44X8SMkbEIOMbdo_39eJF-aPlLi-iUXEVw"
+    // const [clanData, setClanData] = useState({});
+    // const TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjQ2ZjhmNjk3LTRmYzItNDJmOC1hODU0LWU1YzgyNGI0NDA5ZiIsImlhdCI6MTU4NzMxODQ2OCwic3ViIjoiZGV2ZWxvcGVyLzExMTU1OTNkLTgwZDctYThjMS01MTA3LTY1OTQwODIxNDI0MiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3NS4xNjIuNDguMzEiXSwidHlwZSI6ImNsaWVudCJ9XX0.kYiHJ20k12BplKU8M8oBRRv9U-buwtYKH1LiTa6_x1qpqmeRP31E44X8SMkbEIOMbdo_39eJF-aPlLi-iUXEVw"
 
     // async function fetchData() {
     //     const res = await fetch("https://api.clashroyale.com/v1/clans/%23PRURJPJP", {
@@ -36,32 +48,33 @@ export default function App() {
     //     fetchData();
     //     console.log('clan data', clanData);
     // });
-
-    return (
-        <BrowserRouter>
-            <MyNavbar />
-            <div style={BG}>
-                <Switch>
-                    <Route path="/Home">
-                        <Home />
-                    </Route>
-                    <Route path="/Rules">
-                        <Rules />
-                    </Route>
-                    <Route path="/Promotion">
-                        <Promotion />
-                    </Route>
-                    <Route path="/Guides">
-                        <Guides />
-                    </Route>
-                    <Route path="/Tournament">
-                        <Tournament />
-                    </Route>
-                </Switch>
-            </div>
-            <Footer />
-            <Redirect from="/" to="Home" />
-            <ScrollToTop />
-        </BrowserRouter>
-    )
+    render() {
+        return (
+            <BrowserRouter>
+                <MyNavbar />
+                <div style={BG}>
+                    <Switch>
+                        <Route path="/Home">
+                            <Home />
+                        </Route>
+                        <Route path="/Rules">
+                            <Rules />
+                        </Route>
+                        <Route path="/Promotion">
+                            <Promotion />
+                        </Route>
+                        <Route path="/Guides">
+                            <Guides />
+                        </Route>
+                        <Route path="/Tournament">
+                            <Tournament />
+                        </Route>
+                    </Switch>
+                </div>
+                <Footer />
+                <Redirect from="/" to="Home" />
+                <ScrollToTop />
+            </BrowserRouter>
+        )
+    }
 };
