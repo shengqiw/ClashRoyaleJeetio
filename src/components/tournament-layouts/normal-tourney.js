78a348c004743f16d3cff3fdda3b8d2fbe29373b
Shengqi Wang <shengqiw97@gmail.com>
2020-12-30 00:56:01 -0500

    unified page ui look of tourneys

--- src/components/tournament-layouts/normal-tourney.js
+++ src/components/tournament-layouts/normal-tourney.js
@@ -11,17 +11,16 @@ export const normalTourney = () => {
                 <Col className="bg-white rounded" md={{ span: 12 }} xl={{ span: 8, offset: 2 }}>
                     <Row>
                         <Col className="ml-5 mr-5 mt-5 mb-3">
-                            <h2 className="text-center text-primary">Jeetio Tournament</h2>
+                            <h2 className="text-center">Jeetio Tournament</h2>
                             <Divider />
-                            <h4 className="m-3 text-primary">How does it work?</h4>
+                            <h4 className="m-3">How does it work?</h4>
                             <ul>
                                 <li>Wait for a clan message from a founding father</li>
                                 <li>Join the clan tournament</li>
                                 <li>Win as many battles at you can</li>
-                                <li>Founding fathers <b>won't</b> earn rewards</li>
                             </ul>
                             <Divider />
-                            <h4 className="m-3 text-primary">Rewards?</h4>
+                            <h4 className="m-3">Rewards?</h4>
                             <div className="ml-3">
                                 <h5 className="ml-3">Top 3 finishers will earn rewards</h5>
                             </div>
@@ -31,7 +30,7 @@ export const normalTourney = () => {
                                 <li>3rd place gets $10</li>
                             </ul>
                             <Divider />
-                            <h4 className="m-3 text-primary">Prize Options</h4>
+                            <h4 className="m-3">Prize Options</h4>
                             <h5 className="ml-3 mb-1">e-Gift cards (iTunes or Google Play)</h5>
                             <p className="ml-5 mb-1">Purchased as US gift card, if you are from outside the US, it might be hard to redeem</p>
                             <h5 className="ml-3 mb-1">Paypal</h5>
@@ -41,7 +40,7 @@ export const normalTourney = () => {
 
                             <h6 className="ml-3 mb-4">*If you won a reward, email us or message us on discord(check out Deej & Hades page)</h6>
                             <Divider />
-                            <h4 className="mt-5 ml-4 mb-0 text-primary">Sign Ups</h4>
+                            <h4 className="mt-5 ml-4 mb-0">Sign Ups</h4>
                         </Col>
                     </Row>
                     <Row>
