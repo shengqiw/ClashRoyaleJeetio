318e4f85dc8666efefc6882102adf6c7478fbfd8
Shengqi Wang <shengqiw97@gmail.com>
2020-12-30 00:01:56 -0500

    added stream page

--- src/components/stream.js
+++ src/components/stream.js
@@ -3,27 +3,19 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import streamLogo from '../images/deejandhadeslogo.jpg'
-import twitch from '../images/twitch.png'
-import discord from '../images/discord.png'
-import twitter from '../images/twitter.png'
-import youtube from '../images/youtube.png'
+import Divider from './divider'
+import {YT} from './embeded'
+import {mappingAround, YTMap} from '../json/links'
+import {TwitterTweetEmbed} from 'react-twitter-embed'
 
-const twitchUrl = "https://www.twitch.tv/clashwithdeejandhades";
-const discordUrl = "https://discord.gg/bRMb8sB";
-const twitterUrl = "https://twitter.com/DeeJandHades";
-const youtubeUrl = "https://www.youtube.com/channel/UCkMFJCWZNqCmVqIykR5Hn7Q";
+const hakai = (platform) => (
+    mappingAround.get(platform)
+)
 
-const mappingAround = new Map([
-        [twitchUrl, twitch], 
-        [discordUrl, discord], 
-        [twitterUrl, twitter], 
-        [youtubeUrl, youtube]
-    ]);
-
-const clickableImage = (url) => (
+const clickableImage = ({url, image}) => (
     <a href={url} rel="noopener noreferrer" target="_blank">
         <img style={{ height: "110px", maxWidth: "100%"}} 
-            src={mappingAround.get(url)} alt={url} className="rounded hoverzoom" />
+            src={image} alt={url} className="rounded hoverzoom" />
     </a>
 );
 
@@ -34,7 +26,7 @@ export default function Stream() {
                     <Row className='pl-3 pr-3 transparent' style={{ height: '100%' }}>
                         <Col className='m-0 p-0 text-center' style={{height: "100%"}} 
                             md={{ span: 12 }} xl={{ span: 4 }}>
-                            <img style={{ height: "100%", maxWidth: "100%", objectFit: 'cover', objectPosition: 'center center'}} 
+                            <img style={{ width: "100%", maxWidth: "100%", objectFit: 'cover', objectPosition: 'center center'}} 
                                 src={streamLogo} alt='deejandhades' className="rounded" />
                         </Col>
                         <Col className='m-0 text-center' style={{ height: '100%' }}
@@ -48,21 +40,47 @@ export default function Stream() {
                                         <li>Friendly battles with viewers and prized tourneys!</li>
                                         <li>HIGH ENERGY + AWESOME CONTENT</li>
                                     </ul>
-                                    <h6><b>Check Out Their Videos and Social Media Below!</b></h6>
+                                    <h5><b>Check Out Their Videos and Social Media Below!</b></h5>
                                 </Col>
                             </Row>
                             <Row>
                                 <Col>
-                                    {clickableImage(twitchUrl)}
+                                    {clickableImage(hakai('twitch'))}
                                 </Col>
                                 <Col>
-                                    {clickableImage(discordUrl)}
+                                    {clickableImage(hakai('discord'))}
                                 </Col>
                                 <Col>
-                                    {clickableImage(twitterUrl)}
+                                    {clickableImage(hakai('twitter'))}
                                 </Col>
                                 <Col>
-                                    {clickableImage(youtubeUrl)}
+                                    {clickableImage(hakai('youtube'))}
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <Row style={{ height: '100%' }}>
+                        <Divider />
+                    </Row>
+                    <Row>
+                        <Col className="transparentWhite col-10 offset-1 mt-5 pt-5">
+                            <h2 className="text-primary text-center">Checkout Our Jeetio Videos</h2>
+                            <Row>
+                                <Col className="col-10 offset-1">
+                                    <h3 className="mt-4">Cornbread defeats #1 Ranked player on ladder</h3>
+                                    {YT(YTMap.get('beat#1'))}
+                                    <Divider />
+
+                                    <h3 className="mt-4">Gus On CWA</h3>
+                                    <TwitterTweetEmbed tweetId={'1335998284624764928'}/>
+                                    <Divider />
+
+                                    <h3 className="mt-4">Gus Tower Trade Strat</h3>
+                                    <TwitterTweetEmbed tweetId={'1335019407807029249'}/>
+                                    <Divider />
+
+                                    <h3 className="mt-4">First Video Ever Edited - cornbread & jeetchainz</h3>
+                                    {YT(YTMap.get('first-video'))}
                                 </Col>
                             </Row>
                         </Col>
@@ -70,4 +88,3 @@ export default function Stream() {
             </Container>
     );
 }
-            
