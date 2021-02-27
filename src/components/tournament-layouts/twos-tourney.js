268e34e3512c9163d6b50797cd9ea7826268e66a
shengqiw <shengqiw97@gmail.com>
2021-02-27 09:46:32 -0600

    bracket gg

--- src/components/tournament-layouts/twos-tourney.js
+++ src/components/tournament-layouts/twos-tourney.js
@@ -3,12 +3,22 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Divider from '../divider'
+import bracket from '../../images/bracket-feb.PNG'
 
 export const twosTourney = () => {
     return (
         <Container fluid={true}>
             <Row>
                 <Col className="bg-white rounded" md={{ span: 12 }} xl={{ span: 8, offset: 2 }}>
+
+                    <Row>
+                        <Col>
+                            <h3 className='m-5 '>Tourney Bracket</h3>
+                            {/* <h6 className='m-5'>Will be displayed after signups are finished</h6> */}
+                            <img className='img-fluid' src={bracket} alt='poop'></img>
+                        </Col>
+
+                    </Row>
                     <Row>
                         <Col className="m-5">
                             <h2 className="text-center">Jeetio 2s Tournament</h2>
@@ -49,10 +59,10 @@ export const twosTourney = () => {
                             <Divider />
                             <h4 className="ml-3">Prizes for top finishers</h4>
                             <ul>
-                                {/* <li>1st place team gets $40 dollars</li>
-                                <li>2nd place team gets $20 dollars</li>
-                                <li>3rd place team gets $10 dollars</li> */}
-                                <li>Exact $$ amount are still being decided by founders, will update ASAP</li>
+                                <li>1st place team gets $50 dollars</li>
+                                <li>2nd place team gets $30 dollars</li>
+                                <li>3rd place team gets $20 dollars</li>
+                                {/* <li>Exact $$ amount are still being decided by founders, will update ASAP</li> */}
                                 <li>Money will be split 50/50 amongst teamate</li>
 
                             </ul>
@@ -65,20 +75,13 @@ export const twosTourney = () => {
                             <h6 className="ml-3">** We have recently introduced Paypal and Venmo as options **</h6>
                         </Col>
                     </Row>
-                    <Row>
-                        <Col>
-                            <h3 className='m-5 '>Tourney Bracket</h3>
-                            <h6 className='m-5'>Will be displayed after signups are finished</h6>
 
-                        </Col>
-
-                    </Row>
-                    <Row>
+                    {/* <Row>
                         <Col>
                             <h3 className='ml-5'>Sign Ups</h3>
                             <iframe title="December" src="https://docs.google.com/forms/d/e/1FAIpQLSeHleUzny4aDRd_mQrSHejbJzxxQJCmlF4IuY_Pl_re___PVg/viewform?embedded=true" width="100%" height="1950" frameBorder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                         </Col>
-                    </Row>
+                    </Row> */}
 
                 </Col>
             </Row>
