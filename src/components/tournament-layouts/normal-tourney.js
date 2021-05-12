52efdcf54de778e0cf08add4258c25562c2c016c
shengqiw <shengqiw97@gmail.com>
2021-05-11 20:52:39 -0500

    new tourney

--- src/components/tournament-layouts/normal-tourney.js
+++ src/components/tournament-layouts/normal-tourney.js
@@ -45,7 +45,7 @@ export const normalTourney = () => {
                     </Row>
                     <Row>
                         <Col className="col-10 offset-1">
-                            <iframe title='Google forms' src="https://docs.google.com/forms/d/e/1FAIpQLSdE0BybdMvyoKRiQBaSxeaXe4KLBpkKmRRVTugBg11eGbvuNA/viewform?embedded=true" width="100%" height="2200" frameBorder="0">Loading…</iframe>
+                            <iframe title='Google forms' src="https://docs.google.com/forms/d/e/1FAIpQLSeUSFmyECOWbpMy5BxWSswxHBJx6pP0JbFH5FffMtp4SrPncw/viewform?embedded=true" width="100%" height="2200" frameBorder="0">Loading…</iframe>
                         </Col>
                     </Row>
                 </Col>
