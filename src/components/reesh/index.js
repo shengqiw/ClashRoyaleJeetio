178835ce21bb53acdfd50471fdbfc0df74da3af3
shengqiw <shengqiw97@gmail.com>
2021-11-18 21:17:40 -0600

    fetch from server

--- src/components/reesh/index.js
+++ src/components/reesh/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
-import { fetchShit } from './fetchShit'
+import { fetchCornbread, fetchClan } from './fetchShit'
+
+const getAsyncStuff = async () => {
+    const cornbread = await fetchCornbread();
+    const jeetioClan = await fetchClan();
+
+}
 
 export default function funfunfun() {
     console.log('in here');
     return (
         <Container fluid={true}>
-            <Button variant="primary" onClick={fetchShit}>Fetch Cool Shit</Button>
+            <Button variant="primary" onClick={getAsyncStuff}>Fetch Cool Shit</Button>
+
+            
+
         </Container >
     );
 }
