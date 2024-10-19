bc0a337213a3dfe3438d93be3019b2e86a52ca37
Shen Wang <shengqiw97@gmail.com>
2024-10-19 17:40:27 -0500

    BREAKING: THE PURGE OF THE OLD

--- src/components/divider.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-export default function Divider() {
-    return (
-        <div className="bg-warning" style={{ height: "10px" }}>
-        </div >
-    );
-}
--- src/components/embeded.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const YT = (url) => (
-    <iframe title={url} width="100%" height="400px" src={url} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-)
