549d6d9ffcc0d9cbfd81eef087111ed301f00310
Shen Wang <shengqiw97@gmail.com>
2024-10-21 16:31:54 -0500

    empty config

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const nextConfig = {
 //   experimental: {
 //     appDir: true,
 //   },
-  output: "export",
-  // Explicitly set environment variables
-  env: {
-    APP_NAME: process.env.APP_NAME,
-  },
+//   output: "export",
+//   // Explicitly set environment variables
+//   env: {
+//     APP_NAME: process.env.APP_NAME,
+//   },
 };
 
 export default nextConfig;
