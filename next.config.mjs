6a0be6e6d50bb01a248f042580a9373acdd20d4e
Shen Wang <shengqiw97@gmail.com>
2024-10-21 16:21:11 -0500

    change output to export

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
-  output: "standalone",
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   experimental: {
+//     appDir: true,
+//   },
+  output: "export",
   // Explicitly set environment variables
   env: {
     APP_NAME: process.env.APP_NAME,
