133767955dc31c2781b4eb7eed9d8498d751a4fc
Shen Wang <shengqiw97@gmail.com>
2024-10-22 19:17:19 -0500

    add http listener

--- infra/main.tf
+++ infra/main.tf
@@ -119,6 +119,22 @@ resource "aws_lb_listener" "clash_website_listener" {
   }
 }
 
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.clash_website_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
 resource "aws_route53_record" "clash_website_record" {
   zone_id = data.aws_route53_zone.clash_website_zone.zone_id
   name    = "clash-website-jeetio"
