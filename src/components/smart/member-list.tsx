38d913d0cfd29fdc09e263fd49981d4f8e26c883
Shen Wang <shengqiw97@gmail.com>
2024-10-26 18:31:24 -0500

    error checking for bad data

--- src/components/smart/member-list.tsx
+++ src/components/smart/member-list.tsx
@@ -11,39 +11,18 @@ import TrophyIcon from "@/assets/trophy-icon.webp";
 import Image from "next/image";
 
 export const MemberList = async ({ members }: { members: any[] }) => {
-  const fetchMemberDetails = members.map(async (member) => {
-    const userFetch = await fetch(
-      `https://xgyzcbqe9i.execute-api.us-east-1.amazonaws.com/user?id=${member.tag
-        .slice(1)
-        .toUpperCase()}`
-    );
-    const {
-      tag,
-      name,
-      trophies,
-      bestTrophies,
-      wins,
-      losses,
-      battleCount,
-      threeCrownWins,
-      challengeMaxWins,
-      role,
-      donations,
-      donationsReceived,
-      totalDonations,
-      warDayWins,
-      currentPathOfLegendSeasonResult,
-      lastPathOfLegendSeasonResult,
-      bestPathOfLegendSeasonResult,
-    } = await userFetch.json();
-    return {
-      outer: {
-        role,
+  const fetchMemberDetails = members
+    .map(async (member) => {
+      const userFetch = await fetch(
+        `https://xgyzcbqe9i.execute-api.us-east-1.amazonaws.com/user?id=${member.tag
+          .slice(1)
+          .toUpperCase()}`
+      );
+      const {
+        tag,
         name,
         trophies,
-      },
-      inner: {
-        tag,
+        bestTrophies,
         wins,
         losses,
         battleCount,
@@ -54,15 +33,37 @@ export const MemberList = async ({ members }: { members: any[] }) => {
         donationsReceived,
         totalDonations,
         warDayWins,
-      },
-      innerRight: {
         currentPathOfLegendSeasonResult,
         lastPathOfLegendSeasonResult,
         bestPathOfLegendSeasonResult,
-        bestTrophies,
-      },
-    };
-  });
+      } = await userFetch.json();
+      return {
+        outer: {
+          role,
+          name,
+          trophies,
+        },
+        inner: {
+          tag,
+          wins,
+          losses,
+          battleCount,
+          threeCrownWins,
+          challengeMaxWins,
+          role,
+          donations,
+          donationsReceived,
+          totalDonations,
+          warDayWins,
+        },
+        innerRight: {
+          currentPathOfLegendSeasonResult,
+          lastPathOfLegendSeasonResult,
+          bestPathOfLegendSeasonResult,
+          bestTrophies,
+        },
+      };
+    });
 
   const memberDetails = await Promise.all(fetchMemberDetails);
 
@@ -79,75 +80,84 @@ export const MemberList = async ({ members }: { members: any[] }) => {
       </Typography>
       <Grid container>
         <Grid size={{ xs: 10, md: 8 }} offset={{ xs: 1, md: 2 }}>
-          {memberDetails.map((member) => (
-            <Accordion key={member.inner.tag}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                style={{ padding: "0.5rem", marginLeft: "0.5rem" }}
-              >
-                <Typography variant="h5" className="gold">
-                  {member.outer.name}
-                </Typography>
-                &nbsp;
-                <Typography
-                  variant="subtitle2"
-                  mx={2}
-                  color="secondary"
-                  style={{ display: "flex", flexDirection: "column-reverse" }}
+          {memberDetails.map((member) => {
+            if (!member.outer.role) return <div key={'bad-egg'}></div>;
+            return (
+              <Accordion key={member.inner.tag}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  style={{ padding: "0.5rem", marginLeft: "0.5rem" }}
                 >
-                  {member.outer.role.toUpperCase()}
-                </Typography>
-                <Typography
-                  variant="h6"
-                  marginLeft={"auto"}
-                  marginRight={"3rem"}
-                >
-                  {member.outer.trophies}
+                  <Typography variant="h5" className="gold">
+                    {member.outer.name}
+                  </Typography>
                   &nbsp;
-                  <Image
-                    src={TrophyIcon}
-                    alt="Clan Trophies"
-                    width="15"
-                    height="15"
-                  />
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Grid container>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    {Object.entries(member.inner).map(([key, val]) => (
-                      <Typography
-                        key={`${member.outer.name}-${key}`}
-                        variant="body1"
-                      >
-                        {key}: {val}
+                  <Typography
+                    variant="subtitle2"
+                    mx={2}
+                    color="secondary"
+                    style={{ display: "flex", flexDirection: "column-reverse" }}
+                  >
+                    {member.outer.role.toUpperCase()}
+                  </Typography>
+                  <Typography
+                    variant="h6"
+                    marginLeft={"auto"}
+                    marginRight={"3rem"}
+                  >
+                    {member.outer.trophies}
+                    &nbsp;
+                    <Image
+                      src={TrophyIcon}
+                      alt="Clan Trophies"
+                      width="15"
+                      height="15"
+                    />
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Grid container>
+                    <Grid size={{ xs: 12, md: 6 }}>
+                      {Object.entries(member.inner).map(([key, val]) => (
+                        <Typography
+                          key={`${member.outer.name}-${key}`}
+                          variant="body1"
+                        >
+                          {key}: {val}
+                        </Typography>
+                      ))}
+                    </Grid>
+                    <Grid size={{ xs: 12, md: 6 }}>
+                      <Typography variant="h6">
+                        Path of Legend (Ranked)
                       </Typography>
-                    ))}
+                      <Typography variant="body1">
+                        Current Season: League{" "}
+                        {member.innerRight.currentPathOfLegendSeasonResult
+                          ?.leagueNumber || 0}
+                      </Typography>
+                      <Typography variant="body1">
+                        Last Season: League{" "}
+                        {member.innerRight.lastPathOfLegendSeasonResult
+                          ?.leagueNumber || 0}
+                      </Typography>
+                      <Typography variant="body1">
+                        Best Season: League{" "}
+                        {member.innerRight.bestPathOfLegendSeasonResult
+                          ?.leagueNumber || 0}
+                      </Typography>
+                      <Typography variant="h6" mt={2}>
+                        Trophy Road
+                      </Typography>
+                      <Typography variant="body1">
+                        Best Trophies: {member.innerRight.bestTrophies}
+                      </Typography>
+                    </Grid>
                   </Grid>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    <Typography variant="h6">
-                      Path of Legend (Ranked)
-                    </Typography>
-                    <Typography variant="body1">
-                      Current Season: League {member.innerRight.currentPathOfLegendSeasonResult?.leagueNumber || 0}
-                    </Typography>
-                    <Typography variant="body1">
-                      Last Season: League {member.innerRight.lastPathOfLegendSeasonResult?.leagueNumber || 0}
-                    </Typography>
-                    <Typography variant="body1">
-                      Best Season: League {member.innerRight.bestPathOfLegendSeasonResult?.leagueNumber || 0}
-                    </Typography>
-                    <Typography variant="h6" mt={2}>
-                      Trophy Road
-                    </Typography>
-                    <Typography variant="body1">
-                      Best Trophies: {member.innerRight.bestTrophies}
-                    </Typography>
-                  </Grid>
-                </Grid>
-              </AccordionDetails>
-            </Accordion>
-          ))}
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
         </Grid>
       </Grid>
     </Box>
